4caa0f1fd51c7e5e6a1158faa4bf0b4167908a1d
ShadowOfSokol <sokolenkoegor19702002@gmail.com>
2024-01-13 16:20:10 +0200

    add FunkSubtraction

--- Lesson/app/Students/SokolenkoEgor/main.py
+++ Lesson/app/Students/SokolenkoEgor/main.py
@@ -1,9 +1,1 @@
-if __name__ == "__main__":
-
-    # n1 = input("first number: ")
-    # n2 = input("second number: ")
-
-
-    def FunkSubtraction(self,n1, n2):
-        return n1 - n2
-
+print("Hello, World!")
--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import Students.SokolenkoEgor.main as func
 
 # Create your views here.
 from django.http import HttpResponse
@@ -7,7 +6,3 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
-
-n1 = input("first number: ")
-n2 = input("second number: ")
-print(func.FunkSubtraction(n1, n2))
